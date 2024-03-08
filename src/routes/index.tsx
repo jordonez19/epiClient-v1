74ec0fbcdcae40c425038313b60fb67ac0b5b859
xavi <javier19pedraza@gmail.com>
2024-03-07 20:42:03 -0500

    routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import PrivateRoute from './PrivateRoute';
+import { Loader } from '../components/Global/Loader';
 
 //PrivatePages
 const Dashboard = React.lazy(() => import('../containers/Dashboard'));
@@ -8,76 +9,57 @@ const Dashboard1 = React.lazy(() => import('../components/Dashboard/Dashboard-1/
 const UniversitiesContainer = React.lazy(() => import('../containers/Universities'));
 const UniversitiesAction = React.lazy(() => import('../containers/Universities/CreateOrEditUniversities'));
 const SchoolsContainer = React.lazy(() => import('../containers/Schools'));
-const CitiesContainer = React.lazy(() => import('../containers/Cities/ListCities')); 
+const CitiesContainer = React.lazy(() => import('../containers/Cities/ListCities'));
 const ProfileContainer = React.lazy(() => import('../containers/Profile'));
 
 //PublicPages
 const Auth = React.lazy(() => import('../Authentication/auth'));
-const SignIn = React.lazy(() => import('../components/Pages/Authentication/SignIn/SignIn'));
-const SignUp = React.lazy(() => import('../components/Pages/Authentication/SignUp/SignUp'));
+/* const SignUp = React.lazy(() => import('../components/Pages/Authentication/SignUp/SignUp'));*/
 const MainContainer = React.lazy(() => import('../containers/Main'));
 const PublicSchool = React.lazy(() => import('../containers/Main/PublicPage'));
 
 //customPages
 const App = React.lazy(() => import('../shade/layouts/App'));
 const Switcherapp = React.lazy(() => import('../shade/layouts/Switcherapp'));
-const Custompages = React.lazy(() => import('../shade/layouts/custompages'));
 const AuthLogin = React.lazy(() => import('../Authentication/Login'));
 const AuthSignup = React.lazy(() => import('../Authentication/Signup'));
-const ForgotPassword = React.lazy(() => import('../components/Pages/Authentication/ForgotPassword/ForgotPassword'));
-const Lockscreen = React.lazy(() => import('../components/Pages/Authentication/Lockscreen/Lockscreen'));
-const ResetPassword = React.lazy(() => import('../components/Pages/Authentication/ResetPassword/ResetPassword'));
 const Error404 = React.lazy(() => import('../components/Pages/Authentication/404Error/404Error'));
 const Error500 = React.lazy(() => import('../components/Pages/Authentication/500Error/500Error'));
 
+/* const Custompages = React.lazy(() => import('../shade/layouts/custompages'));
+const ForgotPassword = React.lazy(() => import('../components/Pages/Authentication/ForgotPassword/ForgotPassword'));
+const Lockscreen = React.lazy(() => import('../components/Pages/Authentication/Lockscreen/Lockscreen'));
+const ResetPassword = React.lazy(() => import('../components/Pages/Authentication/ResetPassword/ResetPassword')); */
+
 const RoutesConfig = () => (
     <Routes>
-        {/* dominio.com/auth/login */}
         {/* //PublicPages */}
-        <Route index element={<MainContainer />} />
         {/* landing */}
-        <Route path="/" element={<MainContainer />} />
-        <Route path="/ingles" element={<PublicSchool />} />
-        <Route path="/universities" element={<MainContainer />} />
-        <Route path="/porqueepi" element={<MainContainer />} />
-        <Route path="/news" element={<MainContainer />} />
-        {/* auth */}
+        <Route path="/" element={<Suspense fallback={<Loader />}><MainContainer /></Suspense>} />
+        <Route path="/ingles" element={<Suspense fallback={<Loader />}><PublicSchool /></Suspense>} />
+        <Route path="/universities" element={<Suspense fallback={<div>Loading...</div>}><MainContainer /></Suspense>} />
+        <Route path="/porqueepi" element={<Suspense fallback={<div>Loading...</div>}><MainContainer /></Suspense>} />
+        <Route path="/news" element={<Suspense fallback={<div>Loading...</div>}><MainContainer /></Suspense>} />
+        <Route path="/pages/Authentication/404error" element={<Error404 />} />
+        <Route path="/pages/Authentication/500error" element={<Error500 />} />
         <Route path="/auth" element={<Auth />}>
             <Route path="/auth/login" element={<AuthLogin />} />
             <Route path="/auth/signup" element={<AuthSignup />} />
         </Route>
-        {/* //PrivatePages */}
         <Route path="/dashboard" element={<App />}>
-            {/* dashboard main */}
             <Route path="/dashboard1" element={<PrivateRoute element={<Dashboard1 />} />} />
             <Route path="/dashboard" element={<PrivateRoute element={<Dashboard />} />} />
-            {/* schools */}
             <Route path="/dashboard/ingles" element={<PrivateRoute element={<SchoolsContainer />} />} />
-            {/* universities */}
             <Route path="/dashboard/universities" element={<PrivateRoute element={<UniversitiesContainer />} />} />
             <Route path="/dashboard/universities/action" element={<PrivateRoute element={<UniversitiesAction />} />} />
-
-            {/* cities */}
             <Route path="/dashboard/cities" element={<PrivateRoute element={<CitiesContainer />} />} />
-            {/*  */}
             <Route path="/dashboard/porque-epi" element={<PrivateRoute element={<Dashboard />} />} />
             <Route path="/dashboard/news" element={<PrivateRoute element={<Dashboard />} />} />
             <Route path="/dashboard/profile" element={<PrivateRoute element={<ProfileContainer />} />} />
             <Route path="*" element={<AuthLogin />} />
         </Route>
-        {/* //customPages */}
-        <Route path="/" element={<Custompages />}>
-            <Route path="/pages/Authentication/signin" element={<SignIn />} />
-            <Route path="/pages/Authentication/signup" element={<SignUp />} />
-            <Route path="/pages/Authentication/forgotpassword" element={<ForgotPassword />} />
-            <Route path="/pages/Authentication/resetpassword" element={<ResetPassword />} />
-            <Route path="/pages/Authentication/lockscreen" element={<Lockscreen />} />
-            <Route path="/pages/Authentication/404error" element={<Error404 />} />
-            <Route path="/pages/Authentication/500error" element={<Error500 />} />
-            <Route path="*" element={<Error404 />} />
-        </Route>
-
         <Route path="/pages/switcher/switcher-1" element={<Switcherapp />} />
+        <Route path="*" element={<Error404 />} />
     </Routes>
 );
 
